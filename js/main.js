e9d6ae0d95dcd3bab4fa4144f6e3801ae5108cd7
Vadim Nosov <60423807+StrangerEagleAge@users.noreply.github.com>
2024-05-15 16:47:53 +0500

    All files ready!

--- js/main.js
+++ js/main.js
@@ -21,7 +21,8 @@ let object;
 let controls;
 
 //Set which object to render
-let objToRender = 'eye';
+//let objToRender = 'eye';
+let objToRender = 'space_helmet';
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
@@ -52,19 +53,23 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "dino" ? 25 : 500;
+//camera.position.z = objToRender === "dino" ? 25 : 500;
+camera.position.z = objToRender === "space_helmet" ? 3 : 500;
 
 //Add lights to the scene, so we can actually see the 3D model
+//!? Если модель не загружается, то нужно больше источников света!
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
 topLight.position.set(500, 500, 500) //top-left-ish
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
+//const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
+const ambientLight = new THREE.AmbientLight(0xF5F5F5, objToRender === "space_helmet" ? 5 : 1);
 scene.add(ambientLight);
 
 //This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "dino") {
+//if (objToRender === "dino") {
+  if (objToRender === "space_helmet") {
   controls = new OrbitControls(camera, renderer.domElement);
 }
 
@@ -72,12 +77,18 @@ if (objToRender === "dino") {
 function animate() {
   requestAnimationFrame(animate);
   //Here we could add some code to update the scene, adding some automatic movement
-
+  //!? ПОВОРОТ МОДЕЛИ
   //Make the eye move
-  if (object && objToRender === "eye") {
+  //if (object && objToRender === "eye") {
+    if (object && objToRender === "space_helmet") {
     //I've played with the constants here until it looked good 
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+    
+    //object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+    //object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+    object.rotation.y = -1.5 + mouseX / window.innerWidth * 3;
     object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+
+    //object.position.x = ;
   }
   renderer.render(scene, camera);
 }
@@ -89,11 +100,11 @@ window.addEventListener("resize", function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+
 //add mouse position listener, so we can make the eye move
 document.onmousemove = (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 }
-
 //Start the 3D rendering
 animate();
